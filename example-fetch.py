96d6b67ece30f1f2c1a48289990be5e77a9a0610
anhkhoakz <noreply-git@anhkhoakz.simplelogin.com>
2025-06-01 08:38:47 +0700

    Add .env.example and pyproject.toml for environment configuration and project metadata. Update example scripts for improved readability and consistency in HTTP requests.

--- example-fetch.py
+++ example-fetch.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 
-import getopt
 import http.cookiejar
-import sys
 import urllib.parse
 import urllib.request
 from http.cookies import SimpleCookie
 from json import loads as json_loads
-from os import environ
 from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
@@ -15,12 +12,15 @@ env = dotenv_values()
 
 _headers = {"Referer": f"{env['BASE_PROTOCOL']}{env['BASE_URL']}"}
 
+
 class UrllibClient:
     """Simple HTTP Session Client, keeps cookies."""
 
     def __init__(self):
         self.cookie_jar = http.cookiejar.CookieJar()
-        self.opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(self.cookie_jar))
+        self.opener = urllib.request.build_opener(
+            urllib.request.HTTPCookieProcessor(self.cookie_jar)
+        )
         urllib.request.install_opener(self.opener)
 
     def get(self, url, headers={}):
@@ -35,28 +35,43 @@ class UrllibClient:
     def _request(self, request):
         response = self.opener.open(request)
         response.status_code = response.getcode()
-        response.data = response.read().decode('utf-8')
+        response.data = response.read().decode("utf-8")
         return response
+
 
 client, cookie = UrllibClient(), SimpleCookie()
 
-cookie.load(vars(client.get(f"{env['BASE_PROTOCOL']}{env['BASE_URL']}"))['headers']['Set-Cookie'])
-csrftoken = cookie['csrftoken'].value
+cookie.load(
+    vars(client.get(f"{env['BASE_PROTOCOL']}{env['BASE_URL']}"))["headers"][
+        "Set-Cookie"
+    ]
+)
+csrftoken = cookie["csrftoken"].value
 
 ## First, create
 payload = {
-    'csrfmiddlewaretoken': csrftoken,
-    'text': 'test',
-    'metadata' : 'OPTION_DISABLE_VIEWS = true',
+    "csrfmiddlewaretoken": csrftoken,
+    "text": "test",
+    "metadata": "OPTION_DISABLE_VIEWS = true",
 }
-result_create = json_loads(client.post(f"{env['BASE_PROTOCOL']}{env['BASE_URL']}" + '/api/new', payload, headers=_headers).data)
+result_create = json_loads(
+    client.post(
+        f"{env['BASE_PROTOCOL']}{env['BASE_URL']}" + "/api/new",
+        payload,
+        headers=_headers,
+    ).data
+)
 print(result_create)
 
 ## Then, fetch
 payload = {
-    'csrfmiddlewaretoken': csrftoken,
-    'edit_code' : result_create['edit_code'],
+    "csrfmiddlewaretoken": csrftoken,
+    "edit_code": result_create["edit_code"],
 }
-result_fetch = client.post(f"{env['BASE_PROTOCOL']}{env['BASE_URL']}" + f"/api/fetch/{result_create['url_short']}", payload, headers=_headers).data
+result_fetch = client.post(
+    f"{env['BASE_PROTOCOL']}{env['BASE_URL']}"
+    + f"/api/fetch/{result_create['url_short']}",
+    payload,
+    headers=_headers,
+).data
 print(result_fetch)
-
--- example-raw.py
+++ example-raw.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 
-import getopt
 import http.cookiejar
-import sys
 import urllib.parse
 import urllib.request
 from http.cookies import SimpleCookie
 from json import loads as json_loads
-from os import environ
 from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
@@ -15,12 +12,15 @@ env = dotenv_values()
 
 _headers = {"Referer": f"{env['BASE_PROTOCOL']}{env['BASE_URL']}"}
 
+
 class UrllibClient:
     """Simple HTTP Session Client, keeps cookies."""
 
     def __init__(self):
         self.cookie_jar = http.cookiejar.CookieJar()
-        self.opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(self.cookie_jar))
+        self.opener = urllib.request.build_opener(
+            urllib.request.HTTPCookieProcessor(self.cookie_jar)
+        )
         urllib.request.install_opener(self.opener)
 
     def get(self, url, headers={}):
@@ -35,24 +35,35 @@ class UrllibClient:
     def _request(self, request):
         response = self.opener.open(request)
         response.status_code = response.getcode()
-        response.data = response.read().decode('utf-8')
+        response.data = response.read().decode("utf-8")
         return response
 
+
 client, cookie = UrllibClient(), SimpleCookie()
-cookie.load(vars(client.get(f"{env['BASE_PROTOCOL']}{env['BASE_URL']}"))['headers']['Set-Cookie'])
-csrftoken = cookie['csrftoken'].value
+cookie.load(
+    vars(client.get(f"{env['BASE_PROTOCOL']}{env['BASE_URL']}"))["headers"][
+        "Set-Cookie"
+    ]
+)
+csrftoken = cookie["csrftoken"].value
 
 # This example does not work without further adjustments!
 # To use the /raw endpoint you must have a SECRET_RAW_ACCESS_CODE. You can request one from support@rentry.co.
 # Either set this value in each of your page, or use it below as a custom header.
 
-example_url = '10'
+example_url = "10"
 
 payload = {
-    'csrfmiddlewaretoken': csrftoken,
+    "csrfmiddlewaretoken": csrftoken,
 }
 
-_headers['rentry-auth'] = ''
+_headers["rentry-auth"] = ""
 
-result_raw = json_loads(client.post(f"{env['BASE_PROTOCOL']}{env['BASE_URL']}" + f'/api/raw/{example_url}', payload, headers=_headers).data)
+result_raw = json_loads(
+    client.post(
+        f"{env['BASE_PROTOCOL']}{env['BASE_URL']}" + f"/api/raw/{example_url}",
+        payload,
+        headers=_headers,
+    ).data
+)
 print(result_raw)
